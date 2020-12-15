30dfb26dc36fa8bf98f23756c668140838729fc2
ardent <tim.veenendaal@gmail.com>
2020-12-15 20:16:09 +0100

    update install

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+!/bin/bash
 
-sudo apt install zsh
+sudo apt install zsh wget curl
 cd "$(dirname "$0")"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 ./solve-stow.sh bin
 ./solve-stow.sh zsh
+
+chsh -s $(which zsh)
